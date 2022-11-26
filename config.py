e45bb811505cdad8422681aeafbd2e4fc9a7df99
mst <ms_taurus@mail.ru>
2022-11-27 02:03:52 +0300

    enviroment variable

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import os
+from os import environ
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    DB_SERVER = environ.get("DB_SERVER") or "127.0.0.1"
+    DB_LOGIN = environ.get("DB_LOGIN") or "phototur"
+    DB_PASSWORD = environ.get("DB_PASSWORD") or "K554fase6u"
+
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://phototur:K554fase6u@127.0.0.1/phototur?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_LOGIN}:{DB_PASSWORD}@{DB_SERVER}/phototur?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMINS = ["a@b.org"]
     POSTS_PER_PAGE = 25
